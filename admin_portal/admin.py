5344da3c888ca3ecb25879585721866c9f4674fc
mdazukalonwabo <mdazukalonwabo@gmail.com>
2021-01-28 13:37:10 +0200

    admin portal

--- admin_portal/admin.py
+++ admin_portal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, CovidQuestionnaire, LeaveApplication, LifeChoicesMember, LifeChoicesAcademy, LifeChoicesStuff
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(CovidQuestionnaire)
+admin.site.register(LeaveApplication)
+admin.site.register(LifeChoicesMember)
+admin.site.register(LifeChoicesAcademy)
+admin.site.register(LifeChoicesStuff)
