fa4a903980db7e3cbe36ab8e533e38b68b114230
LwandoG <lwandogqobho@gmail.com>
2021-02-03 12:03:18 +0200

    lwando

--- admin_portal/api/views.py
+++ admin_portal/api/views.py
@@ -87,11 +87,11 @@ class MyObtainTokenPairView(TokenObtainPairView):
 
 
 # obtain the users token
-# class CustomObtainAuthToken(ObtainAuthToken):
-#     def post(self, request, *args, **kwargs):
-#         response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
-#         token = Token.objects.get(key=response.data['token'])
-#         return Response({'token': token.key, 'id': token.user_id})
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'id': token.user_id})
 
 
 csrf_protected_method = method_decorator(csrf_protect)
@@ -102,7 +102,7 @@ class LoginAPI(KnoxLoginView, CustomObtainAuthToken):
     serializer_class = AuthTokenSerializer
     parser_classes = [MultiPartParser]
 
-    #@csrf_protected_method
+    @csrf_protected_method
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
