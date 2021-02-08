8945be18d8645dcce0e7b8679075da572582866c
mdazukalonwabo <mdazukalonwabo@gmail.com>
2021-02-08 10:43:30 +0200

    covid questionnaire

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib.auth.decorators import login_required
-from .views import HomePageView
+from .views import HomePageView, CovidQuestionnairePage
 
 
 app_name = 'admin_portal'
@@ -8,5 +8,6 @@ app_name = 'admin_portal'
 
 urlpatterns = [
     path('', login_required(HomePageView.as_view()), name='home'),
+    path('/covid-questionnaire', CovidQuestionnairePage.as_view(), name='covid-questionnaire'),
     path('api/', include('admin_portal.api.urls'))
 ]
--- admin_portal/views.py
+++ admin_portal/views.py
@@ -1,5 +1,22 @@
-from django.views.generic.base import TemplateView
+from datetime import datetime
+
+from django.views.generic.base import TemplateView, View
+from .models import CovidQuestionnaire
 
 
 class HomePageView(TemplateView):
     template_name = "index.html"
+
+
+class CovidQuestionnairePage(View):
+    template_name = "covid_questionnaire.html"
+
+
+def covid_questionnaire_completed(user_id):
+    # queries the database to check if the user has completed the questionnaire today
+    latest_questionnaire = CovidQuestionnaire.objects.get(user_id=user_id)
+    todays_date = datetime.now().date()
+    for i in latest_questionnaire:
+        if todays_date == i.time_signed.date():
+            return True
+    return False
