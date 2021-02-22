ae4c02ca063bce754e93fc0a8b71b1e71700be88
LwandoG <lwandogqobho@gmail.com>
2021-02-22 09:49:17 +0200

    deleted csv's

--- admin_portal/views.py
+++ admin_portal/views.py
@@ -7,7 +7,6 @@ from django.contrib import messages
 
 from .models import CovidQuestionnaire, User, LeaveApplication, LifeChoicesMember, LifeChoicesStuff, LifeChoicesAcademy
 from .forms import CovidForm
-from .writing_to_csv import write_to_covid_csv, write_to_user_csv, write_to_leave_csv
 
 
 class HomePageView(View):
@@ -24,9 +23,6 @@ class HomePageView(View):
             'pending_accounts': User.objects.filter(is_active=False),
             'leaves_applications': LeaveApplication.objects.all()
         }
-        write_to_covid_csv()
-        write_to_user_csv()
-        write_to_leave_csv()
         return render(request, self.template_name, context=context)
 
 
