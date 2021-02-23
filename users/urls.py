2f7c4ee8c10e5c07eab80bbba718589167525ece
mdazukalonwabo <mdazukalonwabo@gmail.com>
2021-02-23 12:44:51 +0200

    profile

--- users/urls.py
+++ users/urls.py
@@ -1,27 +1,24 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import Register, profile, PendingAccounts, ActivatePendingAccount, activate_account, ViewProfile, \
-    AdminPageView, RegistrationConfirmation, update_profile, update_bankingdetails, update_kin_details, update_contact_details
+from .views import Register, PendingAccounts, ActivatePendingAccount, activate_account, ViewProfile, AdminPageView, \
+    RegistrationConfirmation, update_profile, update_bankingdetails, update_kin_details, update_contact_details
 
 
 app_name = 'users'
 
 urlpatterns = [
-    #path('profile/', views.profile, name='profile'),
-    path('register/', views.Register.as_view(), name='register'),
+    path('register/', Register.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('pending-accounts/', views.PendingAccounts.as_view(),
-         name='pending-accounts'),
-    path('pending-accounts/<int:pk>', views.ActivatePendingAccount.as_view(),
-         name='activate-pending-accounts'),
+    path('pending-accounts/', PendingAccounts.as_view(), name='pending-accounts'),
+    path('pending-accounts/<int:pk>', ActivatePendingAccount.as_view(), name='activate-pending-accounts'),
     path('activate-account/<int:pk>', activate_account, name='activate-account'),
     path('view-profile/', ViewProfile.as_view(), name='view-profile'),
     path('admin-page/', AdminPageView.as_view(), name='admin'),
     # path('account-profile/', AccountProfilePageView.as_view(), name='accountprofile'),
     path('registration-confirmation/', RegistrationConfirmation.as_view(), name='registration-confirmation'),
-    path('update-profile/<int:pk>/', update_profile, name='updateprofile'),
-    path('update-banking-details/', update_bankingdetails, name='updatebankingdetails'),
-    path('update-kin-details/', update_kin_details, name='updatekin'),
-    path('update-contact-details/', update_contact_details, name='updatecontact')
+    path('update-profile/<int:pk>/', update_profile, name='update-profile'),
+    path('update-banking-details/', update_bankingdetails, name='update-banking-details'),
+    path('update-kin-details/', update_kin_details, name='update-kin'),
+    path('update-contact-details/', update_contact_details, name='update-contact')
 ]
--- users/views.py
+++ users/views.py
@@ -5,14 +5,11 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib.auth.models import Group
 
-<<<<<<< HEAD
-from .forms import RegisterUserForm, GeneralUserUpdateForm, StaffUpdateForm, StudentUpdateForm, LifeChoicesForm, BankingDetailsForm
+
 from .email_confirmation import activate_email
-from admin_portal.models import User, LifeChoicesMember, LifeChoicesAcademy, LifeChoicesStuff
-=======
 from .forms import RegisterUserForm, BankingDetailsForm, BasicInfoForm, NextOfKinForm, ContactDetailsForm
 from admin_portal.models import User, LifeChoicesMember, LifeChoicesAcademy, BankingDetail
->>>>>>> remotes/heroku/Pillz_user-registration-groups
+
 
 
 class Register(View):
@@ -37,10 +34,7 @@ class Register(View):
     def post(self, request, *args, **kwargs):
         form = RegisterUserForm(request.POST)   # completed form
         if form.is_valid():
-<<<<<<< HEAD
             form.save()
-=======
->>>>>>> remotes/heroku/Pillz_user-registration-groups
             email = form.cleaned_data["email"]
             username = form.cleaned_data.get('username')
             user = User.objects.filter(email=email).first()
@@ -50,102 +44,26 @@ class Register(View):
             # add groups to
             user_group = ''
             if role == "visitor":
-<<<<<<< HEAD
                 group = Group.objects.get(name='visitor')
                 user.groups.add(group)
                 messages.success(request, f'{username} your account has been created! You are now able to log in')
-                return redirect('users:login')
-=======
                 user_group = Group.objects.get(name='visitor')
->>>>>>> remotes/heroku/Pillz_user-registration-groups
+                user.groups.add(user_group)
+                return redirect('users:registration-confirmation')
             elif role == 'business_unit':
                 user_group = Group.objects.get(name='business_unit')
             elif role == '	staff':
                 user_group = Group.objects.get(name='staff')
             else:
-<<<<<<< HEAD
                 group = Group.objects.get(name='student')
                 user.groups.add(group)
-            return redirect('users:registration-confirmation')
-=======
                 user_group = Group.objects.get(name='student')
-
             user.groups.add(user_group)
             messages.success(
                 request, f'{username} your account has been created! You are now able to log in')
             return redirect('users:login')
->>>>>>> remotes/heroku/Pillz_user-registration-groups
         else:
             return render(request, self.template_name, {'form': form})
-
-
-""" def profile(request):
-    user = request.user
-    # load form specific to user
-    user_form = GeneralUserUpdateForm(instance=user)
-<<<<<<< HEAD
-=======
-    if user.groups.filter(name='visitor').exists():
-        user_form = GeneralUserUpdateForm(instance=user)
-    elif user.groups.filter(name='business_unit').exists():
-        user_form = StaffUpdateForm(instance=user)
-    elif user.groups.filter(name='staff').exists():
-        user_form = StaffUpdateForm(instance=user) 
-    elif user.groups.filter(name='student').exists():
-        user_form = StudentUpdateForm(instance=user)
->>>>>>> remotes/heroku/Pillz_user-registration-groups
-
-    if request.method == "GET":
-        if additional_forms(request)[1]:
-            context = {
-                'form': user_form,
-                'life_choices_form': additional_forms(request)[0],
-                'formset': additional_forms(request)[1],
-            }
-        else:
-            context = {
-                'form': user_form,
-            }
-        return render(request, 'profile.html', context=context)
-
-    if request.method == 'POST':
-        user_form = GeneralUserUpdateForm(request.POST, request.FILES, instance=user)
-        # saving the general form
-        if user_form.is_valid():
-            user_form.save()
-            # only users that are life choices can submit this forms
-            if additional_forms(request)[0]:
-                print('im here')
-                # basic details required from a life choices member
-                member = LifeChoicesMember.objects.filter(user=request.user).first()
-                life_choices_form = LifeChoicesForm(request.POST, instance=member)
-                if life_choices_form.is_valid():
-                    life_choices_form.save()
-                if request.user.roles == 'business_unit' or 'staff':
-                    print('im a b or sta')
-                    instance = LifeChoicesStuff.objects.filter(user=member).first()
-                    formset = StaffUpdateForm(request.POST, instance=instance)
-                    if formset.is_valid():
-                        formset.save()
-                elif request.user.roles == 'student':
-                    print('im a stu')
-                    formset = StudentUpdateForm(request.POST, instance=request.user)
-                    if formset.is_valid():
-                        formset.save()
-        messages.success(request, f'account update successfully')
-        return redirect('users:profile') """
-
-
-""" def additional_forms(request):
-    formset = None
-    member = LifeChoicesMember.objects.filter(user=request.user).first()
-    life_choices_form = LifeChoicesForm(instance=member)
-    if request.user.roles == 'business_unit' or 'staff':
-        instance = Bankingdetails.objects.filter(user=member).first()
-        formset = StaffUpdateForm(instance=instance)
-    elif request.user.roles == 'student':
-        formset = StudentUpdateForm(instance=request.user)
-    return life_choices_form, formset """
 
 
 class PendingAccounts(ListView):
@@ -174,11 +92,7 @@ def activate_account(request, pk):
 class ViewProfile(View):
     template_name = 'account_profile.html'
 
-<<<<<<< HEAD
-    def get(self, request):
-=======
     def get(self, *args, **kwargs):
->>>>>>> remotes/heroku/Pillz_user-registration-groups
         context = {}
         form = BankingDetailsForm()
         context['form'] = form
@@ -212,7 +126,7 @@ def update_profile(request, pk):
         form = BasicInfoForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('users:viewprofile')
+            return redirect('users:view-profile')
     return render(request, 'profile/updateprofile.html', context)
 
 
@@ -226,7 +140,7 @@ def update_bankingdetails(request):
         form = BankingDetailsForm(instance=details, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('users:viewprofile')
+            return redirect('users:view-profile')
     return render(request, 'profile/updatebankingdetails.html', context)
 
 
@@ -240,7 +154,7 @@ def update_contact_details(request):
         form = ContactDetailsForm(instance=details, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('users:viewprofile')
+            return redirect('users:view-profile')
     return render(request, 'profile/updatecontactdetails.html', context)
 
 
@@ -254,5 +168,5 @@ def update_kin_details(request):
         form = NextOfKinForm(instance=details, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('users:viewprofile')
+            return redirect('users:view-profile')
     return render(request, 'profile/updatenextofkin.html', context)
